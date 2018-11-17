18fb0ff398f4a2f51878772c0cf7df6cf76ff91e
Matthew Sherman <msherman0801@gmail.com>
2018-11-17 18:25:37 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +11,72 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song = Song.new
+    @@all << song
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.create 
+    song.name = name
+    song
+  end 
+  
+  def self.create_by_name(name)
+    song = self.create 
+    song.name = name
+    song
+  end 
+  
+  def self.find_by_name(name)
+    @@all.find {|i| 
+      if i.name == name
+        name
+      end
+    }
+  end 
+  
+  def self.find_or_create_by_name(song)
+    if self.find_by_name(song)
+      self.find_by_name(song)
+    else 
+      self.create_by_name(song)
+    end
+  end
+  
+  def self.alphabetical
+    self.all.sort_by { |song| song.name}
+  end
+  
+  def self.new_from_filename(file_name)
+    song = self.create_by_name(file_name)
+    out = file_name.split(" - ").each do |i|
+      if i.match(/.mp3/)
+        index = i.index('.')
+        i.slice!(index, 4)
+      end
+    end
+    song = self.new_by_name(out[1])
+    song.artist_name = out[0]
+    song
+  end
+  
+  def self.create_from_filename(file_name)
+    song = self.create_by_name(file_name)
+    out = file_name.split(" - ").each do |i|
+      if i.match(/.mp3/)
+        index = i.index('.')
+        i.slice!(index, 4)
+      end
+    end
+    song = self.create_by_name(out[1])
+    song.artist_name = out[0]
+    song
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end
 end
