af89027d372ba04af06a92409f05149d2877f7de
Paul Ashour <paul.ashour@gmail.com>
2018-11-19 03:14:07 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +11,57 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song.save
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.each do |song|
+      return song if song.name == name 
+    end
+    nil
+  end
+  
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create_by_name(name)
+  end
+  
+  def self.alphabetical
+    @@all.sort_by { |song| song.name }
+  end
+  
+  def self.new_from_filename(filename)
+    info = filename.chomp('.mp3').split(' - ')
+    song = new_by_name(info[1])
+    song.artist_name = info[0]
+    song
+  end
+  
+  def self.create_from_filename(filename)
+    new_from_filename(filename).save
+    @@all.last
+  end
+  
+  def self.destroy_all
+    @@all = []
+  end
+end
 
+def crop_string_between_markers(string, m1, m2)
+  crop = string[/#{Regexp.escape(m1)}(.*?)#{Regexp.escape(m2)}/m, 1]
 end
