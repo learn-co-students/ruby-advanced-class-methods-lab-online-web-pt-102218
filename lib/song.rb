e5caf1fd55d1a8bf551ca3870b0c9e69a5ff77af
Jane Venter <janeanneventer@gmail.com>
2018-11-21 18:41:34 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,70 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    c = self.new
+    c.save
+    c 
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    @name = name
+    song.name = @name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new 
+    @name = name 
+    song.name = name 
+    song.save
+    song
+  end
+  
+  def self.find_by_name(name)
+    result = nil
+    self.all.each do |s|
+      result = s if s.name == name 
+    end 
+    result
+  end
+  
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil 
+      self.create_by_name(name) 
+    else
+      self.find_by_name(name)
+    end
+  end
+  
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+  
+  def self.new_from_filename(filename)
+    song = self.new
+     
+    f = filename.split(" - ")
+    #@artist_name = f[0]
+    #@name = f[1].gsub(".mp3", "")
+    song.artist_name = f[0]
+    song.name = f[1].gsub(".mp3", "")
+    song
+  end
+  
+  def self.create_from_filename(filename)
+    song = self.new
+    f = filename.split(" - ")
+    song.artist_name = f[0]
+    song.name = f[1].gsub(".mp3", "")
+    song.save
+    song
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
 end
+
