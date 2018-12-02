d4524e7e9f13122f737b7e59084cff1cc91d6224
Dakota Clark <bannaray95@gmail.com>
2018-12-02 01:58:21 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,69 @@ class Song
   def self.all
     @@all
   end
-
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(string_name_of_song)
+    song = self.new
+    song.name = string_name_of_song
+    song
+  end
+  
+  def self.create_by_name(string_name_of_song)
+    song = self.new
+    song.name = string_name_of_song
+    song.save
+    song
+  end
+  
+  def self.find_by_name(string_name_of_song)
+    self.all.detect{|song| song.name == string_name_of_song}
+  end
+  
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+    
+  
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+  
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3","")
+    
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+  
+  def self.create_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3","")
+    
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end
+  
   def save
     self.class.all << self
   end
-
+  
+  
+  
 end
